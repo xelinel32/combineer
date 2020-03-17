816b396968e264f67142288eae6fe738bbe18cf1
xelinel32 <artemsedlyar99@gmail.com>
2020-03-17 15:24:12 +0200

    hofix mistakes

---
+++ app/js/common.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    // Match Height
+    // $('').matchHeight();
+
+    // $('#date').mask('99/99/9999');
+    // $('#phone').mask('(999) 999-9999');
+    
+    // Animation
+    // new WOW().init();
+
+    // $(".smooth").on('click', function () {
+    //     var elementClick = $(this).attr("href");
+    //     var destination = $(elementClick).offset().top;
+    //     $("html:not(:animated),body:not(:animated)").animate({
+    //         scrollTop: destination
+    //     }, 800);
+    //     return false;
+    // });
+
+})
