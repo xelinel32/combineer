ab9815e193c3c6fb98cd5429e6b01d9c85d09e15
xelinel32 <artemsedlyar99@gmail.com>
2020-03-17 15:23:41 +0200

    Added html minify and font converter + parcer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
-let preprocessor = 'scss';
-let fileswatch = 'html,htm,txt,json,md,woff2';
-let imageswatch = 'jpg,jpeg,png,webp,svg';
+let preprocessor = 'scss'; // choose preprocessor folder/file ext.
+let fileswatch = 'html,htm,txt,json,md,woff2,woff,eot,ttf'; // watching another file
+let imageswatch = 'jpg,jpeg,png,webp,svg'; // work with img
 
 const { src, dest, parallel, series, watch } = require('gulp');
 const scss = require('gulp-sass');
@@ -12,6 +12,8 @@ const autoprefixer = require('gulp-autoprefixer');
 const imagemin = require('gulp-imagemin');
 const newer = require('gulp-newer');
 const del = require('del');
+const fontconvert = require('gulp-ttf2woff2');
+const htmlmin = require('gulp-htmlmin');
 
 // Local Server
 
@@ -22,17 +24,31 @@ function browsersync() {
   });
 }
 
+// Font Converter ttf to woff2
+
+function fconvert(){
+    return src('app/fonts/*.ttf')
+      .pipe(fontconvert())
+      .pipe(dest('app/fonts/dest'));
+}
+
+// HTML Minify
+
+function htmlpress(){
+    return src('app/*.html')
+      .pipe(htmlmin({ collapseWhitespace: true, useShortDoctype: true}))
+      .pipe(concat('output.html'))
+      .pipe(dest('app'));
+}
+
 // Custom Styles & CSS Libraries
 
 function styles() {
-  return src([
-    'app/' + preprocessor + '/main.*'
-    // 'node_modules/normalize.css/normalize.css'
-  ])
+  return src('app/' + preprocessor + '/main.scss')
     .pipe(eval(preprocessor)())
-    .pipe(concat('main.min.css'))
+    .pipe(concat('style.min.css'))
     .pipe(
-      autoprefixer({ overrideBrowserslist: ['last 2 versions'], grid: true })
+      autoprefixer({ overrideBrowserslist: ['last 2 versions'], grid: true, cascade: false })
     )
     .pipe(cleancss({ level: { 1: { specialComments: 0 } } }))
     .pipe(dest('app/css'))
@@ -43,16 +59,20 @@ function styles() {
 
 function scripts() {
   return src([
-    'node_modules/jquery/dist/jquery.min.js', // example (npm i --save-dev plugin)
-    'app/script/common.js' // end point
+    './node_modules/jquery/dist/jquery.min.js', // example (npm i --save-dev plugin)
+    'app/js/common.js'
   ])
     .pipe(concat('common.min.js'))
-    .pipe(uglify()) // Minify JS (opt.)
-    .pipe(dest('app/script'))
+    .pipe(
+      uglify({
+        toplevel: true
+      })
+    ) // Minify JS (opt.)
+    .pipe(dest('app/js'))
     .pipe(browserSync.stream());
 }
 
-// Image
+// Image minify
 
 function images() {
   return src('app/img/src/**/*')
@@ -62,7 +82,7 @@ function images() {
 }
 
 function cleanimg() {
-  return del('app/img/dest/**/*', { force: true });
+  return del('build/img/dest/**/*',  { force: true });
 }
 
 // Watching
@@ -71,13 +91,15 @@ function startwatch() {
   watch('app/' + preprocessor + '/**/*', parallel('styles'));
   watch(['app/**/*.js', '!app/js/*.min.js'], parallel('scripts'));
   watch(['app/**/*.{' + imageswatch + '}'], parallel('images'));
-  watch(['app/**/*.{' + fileswatch + '}']).on('change', browserSync.reload);
+  watch(['./*.{' + fileswatch + '}']).on('change', browserSync.reload);
 }
 
 exports.browsersync = browsersync;
-exports.assets = series(cleanimg, styles, scripts, images);
+exports.assets = series(cleanimg, fconvert, htmlpress, styles, scripts, images);
 exports.styles = styles;
 exports.scripts = scripts;
 exports.images = images;
+exports.htmlpress = htmlpress;
+exports.fconvert = fconvert;
 exports.cleanimg = cleanimg;
-exports.default = parallel(images, styles, scripts, browsersync, startwatch);
+exports.default = parallel(images, htmlpress, fconvert, styles, scripts, browsersync, startwatch);
